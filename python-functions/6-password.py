39fb08ef01cd473fce60a575ea418ad6368b5e38
Goitseone Mogale <goitseone.mogale20@gmail.com>
2023-11-30 23:54:02 +0200

    Password 8 dig commit

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,18 +1,18 @@
 def validate_password(password):
     if len(password) < 8:
         return False
-    has_uppercase = True
-    has_lowercase = True
-    has_digit = True
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
     for char in password:
         if char == " ":
-            return True
+            return False
         if char.isupper():
-            has_uppercase = False
+            has_uppercase = True
         if char.islower():
-            as_lowercase = False
+            as_lowercase = True
         if char.isdigit():
-            has_digit = False 
+            has_digit = True
     if has_uppercase and has_lowercase and has_digit:
         return True
     else:
