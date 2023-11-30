8254ddb77cc7b14252d2010577c262b7d938baa3
Goitseone Mogale <goitseone.mogale20@gmail.com>
2023-11-30 21:45:51 +0200

    try commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,5 +4,5 @@ if number > 5:
  print("Last digit of"+ number + "is" + number + "and is greater than 5\n")
 elif number < 6 and not 0:
  print("Last digit of" + number + "is" + number + "and is less than 6 and not 0")
-elif number == 0:
+else:
  print("Last digit of"+ number + "is"+ number + "and is 0\n")
