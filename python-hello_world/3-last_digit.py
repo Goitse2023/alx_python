ec4ec334a6b12c48b544ff66c215ddc1227b905e
Goitseone Mogale <goitseone.mogale20@gmail.com>
2023-11-30 21:03:19 +0200

    last digit commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,6 @@
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of 5247 is 7 and is greater than 5")
-print("Last digit of -9200 is 0 and is 0")
-print("Last digit of 4205 is 5 and is less than 6 and not 0")
+print("Last digit of 98 is 8 and is greater than 5\n")
+print("Last digit of -98 is -8 and is less than 6 and not 0\n")
+print("Last digit of 0 is 0 and is 0\n")
+print("Last digit of 4205 is 5 and is less than 6 and not 0\n")
