de414c3dd52164eefad6212f3268930cdbb9d2af
Goitseone Mogale <goitseone.mogale20@gmail.com>
2023-11-30 21:40:00 +0200

    try last digit commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,8 +1,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 5:
- print("Last digit of 98 is 8 and is greater than 5\n")
+ print("Last digit of"+ number + "is" + number + "and is greater than 5\n")
 elif number < 6 and not 0:
- print("Last digit of -98 is -8 and is less than 6 and not 0\n")
+ print("Last digit of" + number + "is" + number + "and is less than 6 and not 0")
 elif number == 0:
- print("Last digit of 0 is 0 and is 0\n")
+ print("Last digit of"+ number + "is"+ number + "and is 0\n")
