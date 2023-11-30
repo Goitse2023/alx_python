cd717c25b6e13695e400c357531abbca61f00b04
Goitseone Mogale <goitseone.mogale20@gmail.com>
2023-11-30 21:22:12 +0200

    digit last commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,8 @@
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of 98 is 8 and is greater than 5\n")
-print("Last digit of -98 is -8 and is less than 6 and not 0\n")
-print("Last digit of 0 is 0 and is 0\n")
-print("Last digit of 4205 is 5 and is less than 6 and not 0\n")
+if number > 5:
+ print("Last digit of 98 is 8 and is greater than 5\n")
+elif number < 6 and not 0:
+ print("Last digit of -98 is -8 and is less than 6 and not 0\n")
+elif number == 0:
+ print("Last digit of 0 is 0 and is 0\n")
