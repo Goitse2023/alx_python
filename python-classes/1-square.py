95044d4e9931202648293f02c242d98f44c325c9
Goitseone Mogale <goitseone.mogale20@gmail.com>
2024-02-13 15:30:32 +0200

    Corrected emphasis Second Class Square Commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -17,33 +17,33 @@ class Square:
         self.__size = size
 
 my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+print(f"Type of my_square_1: {type(my_square_1)}")
+print(f"Attributes of my_square_1: {my_square_1.__dict__}")
 
 my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+print(f"Type of my_square_2: {type(my_square_2)}")
+print(f"Attributes of my_square_2: {my_square_2.__dict__}")
 
 try:
     print(my_square_1.size)
-except Exception as e:
+except AttributeError as e:
     print(e)
 
 try:
-    print(my_square_1.__size)
-except Exception as e:
+    print(my_square_1._Square__size)
+except AttributeError as e:
     print(e)
 
 try:
     my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
+    print(f"Type of my_square_3: {type(my_square_3)}")
+    print(f"Attributes of my_square_3: {my_square_3.__dict__}")
 except Exception as e:
     print(e)
 
 try:
     my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
+    print(f"Type of my_square_4: {type(my_square_4)}")
+    print(f"Attributes of my_square_4: {my_square_4.__dict__}")
 except Exception as e:
     print(e)
